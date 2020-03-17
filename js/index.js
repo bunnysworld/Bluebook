18b75710005732a8641d15227d0d143f567e11f1
bunnysword <akashdwivedi563@gmaul.com>
2020-03-17 21:43:04 +0530

    submenu on tap  only

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ $(".hamburger").click(function(){
     $(".wrapper").toggleClass("collapse");
     $("i", this).toggleClass("fas fa-window-close fas fa-search");
  });
+
+$(".sub-nav").click(function(){
+    $(this).toggleClass('tap');
+}); 
