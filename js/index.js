c15a667d58c65cf3ad05a94e34c31bb12146e7e3
bunnysword <akashdwivedi563@gmaul.com>
2020-03-18 21:46:06 +0530

    bg color in submenus

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(".hamburger").click(function(){
     $(".wrapper").toggleClass("collapse");
     $("i", this).toggleClass("fas fa-window-close fas fa-search");
- });
+});
 
 $(".sub-nav").click(function(){
     $(this).toggleClass('tap');
