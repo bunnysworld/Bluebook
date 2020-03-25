79f9d1e997eb0cb1a42e9458f7055b4a6f3e57f3
bunnysword <akashdwivedi563@gmaul.com>
2020-03-25 21:05:35 +0530

    deleted the .html extensions

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bluebook</title>
+    <script src="https://kit.fontawesome.com/11a408d726.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <div class="top_navbar">
+            <div class="hamburger">
+                
+                <span class="icon search"><i class="fas fa-window-close"></i></span>
+                
+            </div>
+            <div class="top_menu">
+                <div class="logo">
+                    Bluebook
+                </div>
+                  
+                <a href="form.php" id="logbtn">
+                    <button type="button" class="btn btn-outline-primary"></button>
+                    <span class="icon"><i class="fas fa-sign-in-alt ml-1"></i></span>
+             </a>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <ul class="side-ul">
+                <li><a href="#">
+                    <span class="title">
+                        <form action="">
+                            <input class="searchbox" type="text" autocomplete="off" placeholder="Search.." name="search">
+                            <button class="searchbox" type="submit"><i class="fa fa-search"></i></button>
+                        </form>
+                    </span>
+                </a></li>
+                <li class="sub-nav"><a href="#">
+                        <span class="icon"><i class="fab fa-java"></i></span>
+                        <span class="title">java</span>
+                        <span class="sub-arrow"></span>
+                    </a>
+                    <ul>
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+                    </ul>
+                </li>
+                <li class="sub-nav"><a href="#">
+                        <span class="icon"><i class="fab fa-grav"></i></span>
+                        <span class="title">Physics</span>
+                    </a>
+                    <ul>
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+
+                        <a href="#">
+                            <span class="icon"><i class="fas fa-sticky-note"></i></span>
+                            <span class="title">java-1</span>
+                        </a>
+                    </ul>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-atom"></i></i></span>
+                        <span class="title">Chemistry</span>
+                    </a></li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-digital-tachograph"></i></span>
+                        <span class="title">Digital Electronics</span>
+                    </a></li>
+                <li><a href="#">
+                        <span class="icon"><i class="fab fa-apple"></i></span>
+                        <span class="title">Operating System</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+                <li><a href="#">
+                        <span class="icon"><i class="fas fa-square-root-alt"></i></span>
+                        <span class="title">Maths</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+<script src="vendor/jquery/jquery-3.4.1.min.js"></script>
+<script src="js/index.js."></script>
+
+
+</html>
